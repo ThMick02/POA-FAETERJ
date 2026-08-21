19fb21d44ef73fb924b9852611dd3a694242d4fd
Thiago <thiagotavares0202@gmail.com>
2026-08-21 12:43:46 -0300

    Reajuste de Funções

--- ap1/Main.java
+++ ap1/Main.java
@@ -5,19 +5,19 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         String caminhoBinario = "hospital_dados.ser";
-
+        
         Recepcao recepcao = new Recepcao("Hospital Central");
-        Paciente p1 = new Paciente(101, "Maria Souza", "111.222.333-44", "A01", arqDiag);
+
         File arqDiag = GerenciadorArquivo.criarDiagnostico(
                 "diagnostico_paciente_1.txt",
                 "Diagnóstico: Quadro gripal leve. Indicação: Repouso e hidratação."
         );
+        Paciente p1 = new Paciente(101, "Maria Souza", "111.222.333-44", "A01", arqDiag);
 
         recepcao.cadastrarPaciente(p1);
 
         System.out.println("\n--- TESTE 1: SERIALIZANDO ---");
         GerenciadorArquivo.serRecepcao(caminhoBinario, recepcao);
-
         System.out.println("\n--- TESTE 2: DESSERIALIZANDO E LENDO ---");
         Recepcao restaurada = GerenciadorArquivo.desRecepcao(caminhoBinario);
 
